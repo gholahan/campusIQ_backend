6c8b2cf8c1444f810b2e23e3ed32776c8b7ffc96
Ibraheem <abdulazeezibraheem68@gmail.com>
2026-05-11 06:51:32 +0100

    schema change in  take google singup

--- Backend/app/features/auth/schema.py
+++ Backend/app/features/auth/schema.py
@@ -4,5 +4,5 @@ from app.common.enums import UserRole
 
 class SyncUserRequest(BaseModel):
     role: UserRole
-    first_name: str
-    last_name: str
+    first_name: str | None = None
+    last_name: str | None = None
